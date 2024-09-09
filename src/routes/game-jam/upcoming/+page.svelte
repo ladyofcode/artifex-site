1f8c067c2931b24c85490a677e100994ffd5d693
Yang Li <u7764210@anu.edu.au>
2024-09-09 22:11:43 +1000

    Add submission block

--- src/routes/game-jam/upcoming/+page.svelte
+++ src/routes/game-jam/upcoming/+page.svelte
@@ -1,18 +1,28 @@
 <script>
 	import Description from '$lib/components/game-jam-template/Description.svelte';
 	import Event from '$lib/components/game-jam-template/Event.svelte';
-	let mockDesc =
+	const mockDesc =
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut' +
 		'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' +
 		'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in' +
 		'voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat' +
 		'non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-	let mockImageSrc = '/images/piece_by_piece/pbp3.png';
-	let mockEvents = [
+	const mockImageSrc = '/images/piece_by_piece/pbp3.png';
+	const mockEvents = [
 		{ date: new Date('2024-9-5 17:00'), location: 'Somewhere', title: 'Event 1' },
 		{ date: new Date('2024-9-10 19:00'), location: 'Somewhere', title: 'Event 2' },
 		{ date: new Date('2024-9-12 12:00'), location: 'Somewhere', title: 'Event 3' }
 	];
+  const mockGuideBullets = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "laboris nisi ut aliquip ex ea commodo consequat.",
+    "voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "on proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  ]
+  const mockSubmissionDesc = 
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut' +
+		'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' +
+		'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.';
 </script>
 
 <div class="template-wrapper">
@@ -21,8 +31,27 @@
 			<h1>Game Jam 2025</h1>
 		</div>
 		<Description description={mockDesc} imageSrc={mockImageSrc}></Description>
-		<a class="link-button" href="google.com" tabindex="0">Read the rules</a>
+
+		<a class="link-button" href="rule-link" tabindex="0">Read the rules</a>
+
     <Event events={mockEvents}></Event>
+    
+    <div class="guide-wrapper">
+      <h2>Guide</h2>
+      <p>Below is advice to help you with the best possible outcome:</p>
+      <ul>
+        {#each mockGuideBullets as bullet}
+          <li><p>{bullet}</p></li>
+        {/each}
+      </ul>
+    </div>
+
+    <div class="submission-wrapper">
+      <h2>Submission</h2>
+      <p>{mockSubmissionDesc}</p>
+      <br>
+      <a class="link-button" href="submit-link">Submit game</a>
+    </div>
 	</div>
 </div>
 
@@ -32,9 +61,22 @@
 		color: black;
 	}
 
+  :global(h2),
+  :global(h3){
+    font-family: 'Jura Variable', Arial, Helvetica, sans-serif;
+		color: black;
+		margin: 0;
+  }
+	:global(p) {
+		font-family: 'Roboto', Arial, Helvetica, sans-serif;
+		color: black;
+		margin: 0;
+	}
+
 	.template-wrapper {
 		display: flex;
 		justify-content: center;
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
 	}
 
 	.template-wrapper > div {
@@ -47,10 +89,45 @@
 	.link-button {
 		all: unset;
 		padding-block: 10px;
-		padding-inline: 60px;
+		padding-inline: 70px;
 		border-radius: 5px;
-		background-color: #95d8db;
+		background-color: #EEEEEE;
 		user-select: none;
 		cursor: pointer;
+    margin-block: 20px;
 	}
+
+  .link-button:hover{
+    background-color: #CCCCCC;
+  }
+
+  .link-button:focus{
+    background-color: #444444;
+    color: #FFFFFF;
+  }
+
+  .guide-wrapper{
+    margin-block: 20px;
+  }
+
+  .guide-wrapper p{
+    padding-inline-start: 4px;
+    margin-block-start: 10px;
+  }
+
+  .guide-wrapper ul{
+    padding-block-start: 10px;
+    padding-inline-start: 26px;
+  }
+
+  .guide-wrapper li{
+    color: black;
+    margin-block-end: 10px;
+  }
+
+  .submission-wrapper p{
+    padding-inline-start: 4px;
+    max-width: 700px;
+    margin-block: 10px;
+  }
 </style>

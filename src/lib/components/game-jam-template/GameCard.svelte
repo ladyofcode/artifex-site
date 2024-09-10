28a268e38fe0085ec0aeaf68ccbdf0d1416a52c8
Yang Li <u7764210@anu.edu.au>
2024-09-10 23:07:47 +1000

    Add styles partly

--- src/lib/components/game-jam-template/GameCard.svelte
+++ src/lib/components/game-jam-template/GameCard.svelte
@@ -1,37 +1,131 @@
 <script>
-  export let images; // : string[]
-  export let gameName; // : string
-  export let members; // : string[]
+	import { gsap } from 'gsap';
+	import { onMount } from 'svelte';
+	import PhotoSwipeLightbox from 'photoswipe/lightbox';
+	import 'photoswipe/style.css';
+
+	export let images; // : string[]
+	export let gameName; // : string
+	export let members; // : string[]
+	export let imgWidth = 300;
+
+	const imageCount = images.length;
+	let currentIndex = 0;
+
+	function slideImage(newIndex) {
+		gsap.to('.gallery', {x: newIndex * -imgWidth});
+    currentIndex = newIndex;
+	}
+
+	onMount(() => {
+		let lightbox = new PhotoSwipeLightbox({
+			gallery: '#gallery',
+			children: 'a',
+			pswpModule: () => import('photoswipe')
+		});
+		lightbox.on('contentActivate', ({ content }) => slideImage(content.index));
+		lightbox.init();
+	});
 </script>
 
 <div class="card-wrapper">
-  <div class="gallery">
-
-  </div>
-  <h2>{gameName}</h2>
-  <ul>
-    {#each members as member}
-    <li>{member}</li>
-    {/each}
-  </ul>
+	<div class="gallery-wrapper">
+    <div class="controller">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+      </svg>      
+    </div>
+		<div class="gallery" id="gallery">
+			{#each images as image}
+				<a
+					class="image-wrapper"
+					href={image}
+					data-pswp-width={1024}
+					data-pswp-height={768}
+					rel="noreferrer"
+				>
+					<img src={image} alt="" />
+				</a>
+			{/each}
+		</div>
+	</div>
+	<h2>{gameName}</h2>
+  <h3>Created by</h3>
+	<ul>
+		{#each members as member}
+			<li>{member}</li>
+		{/each}
+	</ul>
 </div>
 
 <style>
-  .card-wrapper {
-    width: 320px;
-    padding-inline: 10px;
-    padding-block: 20px;
+	:root {
+		--image-width: 300px;
+	}
+
+	.card-wrapper {
+		width: 320px;
+		padding-inline: 10px;
+		padding-block: 10px;
+		border-radius: 14px;
+		background-color: #dddddd;
+	}
+
+	.gallery-wrapper {
+		border-radius: 4px;
+		width: 100%;
+		aspect-ratio: 4/3;
+		background-color: #eeeeee;
+		overflow: clip;
+    position: relative;
+	}
+
+  .controller {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255,255,255,0.8);
+    pointer-events: none;
+    z-index: 10;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  svg {
+    size: 1px;
   }
 
   .gallery {
-    width: 100%;
-    aspect-ratio: 4/3;
+    display: flex;
+		flex-wrap: nowrap;
   }
 
-  .gallery img{
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-    object-position: center;
-  }
+	.image-wrapper {
+		width: var(--image-width);
+		aspect-ratio: 4/3;
+		flex-shrink: 0;
+		flex-grow: 0;
+	}
+
+	.gallery img {
+		width: 100%;
+		height: 100%;
+		object-fit: contain;
+		object-position: center;
+	}
+
+	ul {
+		padding-left: 24px;
+	}
+
+	li {
+		font-family: var(--ff-body);
+		color: black;
+	}
 </style>
--- src/lib/components/game-jam-template/PastJamTemplate.svelte
+++ src/lib/components/game-jam-template/PastJamTemplate.svelte
@@ -1,79 +1,94 @@
 <script>
-	import GameGallery from "$lib/components/game-jam-template/GameGallery.svelte";
-  import WinnerGallery from "$lib/components/game-jam-template/WinnerGallery.svelte";
-	import Description from "./Description.svelte";
-	import Event from "./Event.svelte";
+	import GameGallery from '$lib/components/game-jam-template/GameGallery.svelte';
+	import WinnerGallery from '$lib/components/game-jam-template/WinnerGallery.svelte';
+	import Description from './Description.svelte';
+	import Event from './Event.svelte';
 	import Sponsors from '$lib/components/game-jam-template/Sponsors.svelte';
+	import GameCard from './GameCard.svelte';
 
-  export let jamTitle; // : string
-  export let jamTheme; // : string
+	export let jamTitle; // : string
+	export let jamTheme; // : string
 	export let description; // : { description: string, imageSrc: string }
 	export let events; // : { date : Date, location: string, title: string }[]
 	export let awards; // : {award: string, winner: string, image: {src: string, width: number, height: number}, url: string}[]
-  export let galleryImages; // : {src: string, width: number, height: number}[]
+	export let galleryImages; // : {src: string, width: number, height: number}[]
 	export let sponsors; // : { name: string, imageSrc: string, url: string }[]
+
+	const mockGame = {
+		images: [
+			'/game-jam/images/2024/games/cant_be_late_2024.png',
+			'/game-jam/images/2024/games/cant_be_late_2024_1.png',
+			'/game-jam/images/2024/games/cant_be_late_2024_2.png',
+			'/game-jam/images/2024/games/cant_be_late_2024_3.png',
+		],
+    gameName: "Can't Be Late",
+    members: ['Idkg Uodsf', 'Vide Odsge', 'Poigd', 'Qjdi Xili'],
+	};
 </script>
 
 <div class="template-wrapper">
-  <div>
-    <div class="jam-title">
-      <h1>{jamTitle}</h1>
-      <h2 style="margin-bottom: 30px;">Theme: {jamTheme}</h2>
-    </div>
-    
+	<div>
+		<div class="jam-title">
+			<h1>{jamTitle}</h1>
+			<h2 style="margin-bottom: 30px;">Theme: {jamTheme}</h2>
+		</div>
 
-    <Description description={description.description} imageSrc={description.imageSrc}></Description>
+		<Description description={description.description} imageSrc={description.imageSrc}
+		></Description>
 
-    <Event events={events} isActive={false}></Event>
+		<Event {events} isActive={false}></Event>
 
-    <WinnerGallery awards={awards}></WinnerGallery>
-    <GameGallery images={galleryImages}></GameGallery>
+		<WinnerGallery {awards}></WinnerGallery>
+		<GameGallery images={galleryImages}></GameGallery>
 
-    <Sponsors sponsors={sponsors}></Sponsors>
-  </div>
+		<Sponsors {sponsors}></Sponsors>
+
+		<GameCard images={mockGame.images} gameName={mockGame.gameName} members={mockGame.members}></GameCard>
+	</div>
 </div>
 
-
-
 <style>
-  .jam-title {
-    margin-block-start: var(--space-md);
-    margin-block-end: var(--space-xxxxl);
-  }
+	.jam-title {
+		margin-block-start: var(--space-md);
+		margin-block-end: var(--space-xxxxl);
+	}
 
-  .jam-title > h1 {
-    font-size: 3rem;
-  }
+	.jam-title > h1 {
+		font-size: 3rem;
+	}
 
-  .jam-title > h2 {
-    padding-inline-start: var(--space-xs);
-  }
+	.jam-title > h2 {
+		padding-inline-start: var(--space-xs);
+	}
 
-  :global(h1), :global(h2), :global(h3){
-    color: black;
-    margin: 0;
-  }
+	:global(h1),
+	:global(h2),
+	:global(h3) {
+		color: black;
+		margin: 0;
+	}
 
-  :global(p){
-    color: black;
-    font-family: var(--ff-body);
-    margin: 0;
-  }
+	:global(p) {
+		color: black;
+		font-family: var(--ff-body);
+		margin: 0;
+	}
 
-  .template-wrapper {
-    display: flex;
-    justify-content: center;
-    padding-inline: var(--space-xxl);
-  }
+	.template-wrapper {
+		display: flex;
+		justify-content: center;
+		padding-inline: var(--space-xxl);
+    background-color: white;
+	}
 
-  .template-wrapper > div {
-    display: flex;
-    flex-direction: column;
-    gap: var(--space-xl);
-  }
+	.template-wrapper > div {
+		display: flex;
+		flex-direction: column;
+		gap: var(--space-xl);
+	}
 
-  :root {
-    --gallery-width: 1000px;
-    --title-margin-bottom: var(--space-sm);
-  }
+	:root {
+		--gallery-width: 1000px;
+		--title-margin-bottom: var(--space-sm);
+	}
 </style>

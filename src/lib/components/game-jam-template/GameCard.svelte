aa7d8b22a5f44a27cb97be3b3f5bbb6ff96100f5
Yang Li <u7764210@anu.edu.au>
2024-09-10 22:00:34 +1000

    Init GameCard component

---
+++ src/lib/components/game-jam-template/GameCard.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let images; // : string[]
+  export let gameName; // : string
+  export let members; // : string[]
+</script>
+
+<div class="card-wrapper">
+  <div class="gallery">
+
+  </div>
+  <h2>{gameName}</h2>
+  <ul>
+    {#each members as member}
+    <li>{member}</li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .card-wrapper {
+    width: 320px;
+    padding-inline: 10px;
+    padding-block: 20px;
+  }
+
+  .gallery {
+    width: 100%;
+    aspect-ratio: 4/3;
+  }
+
+  .gallery img{
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    object-position: center;
+  }
+</style>

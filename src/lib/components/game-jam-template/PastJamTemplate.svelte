8fa279d75e2bb5d3a3a92c4c0503029e7aea5340
Yang Li <u7764210@anu.edu.au>
2024-09-10 17:00:49 +1000

    Refactor PastJamTemplate component

--- src/lib/components/game-jam-template/PastJamTemplate.svelte
+++ src/lib/components/game-jam-template/PastJamTemplate.svelte
@@ -5,69 +5,31 @@
 	import Event from "./Event.svelte";
 	import Sponsors from '$lib/components/game-jam-template/Sponsors.svelte';
 
-  const mockDesc =
-		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut' +
-		'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco' +
-		'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in' +
-		'voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat' +
-		'non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-	const mockImageSrc = '/images/piece_by_piece/pbp3.png';
-
-	const mockDescription = {description: mockDesc, imageSrc: mockImageSrc};
-
-  const mockEvents = [
-		{ date: new Date('2023-9-5 17:00'), location: 'Somewhere', title: 'Event 1' },
-		{ date: new Date('2023-9-10 19:00'), location: 'Somewhere', title: 'Event 2' },
-		{ date: new Date('2023-9-12 12:00'), location: 'Somewhere', title: 'Event 3' }
-	];
-
-  const mockAwards = [
-    { award: "Best Bishop Award", winner: "Bishop", image: {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540}, url: "bishop" },
-    { award: "Best Bishop Award", winner: "Bishop", image: {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540}, url: "bishop" },
-    { award: "Best Bishop Award", winner: "Bishop", image: {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540}, url: "bishop" },
-    
-    { award: "Best Bishop Award", winner: "Bishop", image: {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540}, url: "bishop" },
-    
-    { award: "Best Bishop Award", winner: "Bishop", image: {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540}, url: "bishop" },
-  ]
-
-  const mockGalleryImages = [
-    {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540},
-    {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540},
-    {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540},
-    {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540},
-    {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540},
-    {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540},
-    {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540},
-    {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540},
-    {src: '/images/piece_by_piece/pbp14.png', width: 960, height: 540},
-  ];
-
-  const mockSponsorData = [
-		{ name: 'Knight', imageSrc: '/images/piece_by_piece/pbp7.png', url: "https://artifexanu.com/" },
-		{ name: 'King', imageSrc: '/images/piece_by_piece/pbp10.png', url: "https://artifexanu.com/" },
-    { name: 'The Honorable Bishop the Third', imageSrc: '/images/piece_by_piece/pbp14.png', url: "https://artifexanu.com/" },
-		{ name: 'Parry', imageSrc: '/images/piece_by_piece/pbp4.png', url: "https://artifexanu.com/" },
-	];
-
+  export let jamTitle; // : string
+  export let jamTheme; // : string
+	export let description; // : { description: string, imageSrc: string }
+	export let events; // : { date : Date, location: string, title: string }[]
+	export let awards; // : {award: string, winner: string, image: {src: string, width: number, height: number}, url: string}[]
+  export let galleryImages; // : {src: string, width: number, height: number}[]
+	export let sponsors; // : { name: string, imageSrc: string, url: string }[]
 </script>
 
 <div class="template-wrapper">
   <div>
     <div class="jam-title">
-      <h1>Game Jam 2024</h1>
-      <h2 style="margin-bottom: 30px;">Theme: Time loop</h2>
+      <h1>{jamTitle}</h1>
+      <h2 style="margin-bottom: 30px;">Theme: {jamTheme}</h2>
     </div>
     
 
-    <Description description={mockDescription.description} imageSrc={mockDescription.imageSrc}></Description>
+    <Description description={description.description} imageSrc={description.imageSrc}></Description>
 
-    <Event events={mockEvents} isActive={false}></Event>
+    <Event events={events} isActive={false}></Event>
 
-    <WinnerGallery awards={mockAwards}></WinnerGallery>
-    <GameGallery images={mockGalleryImages}></GameGallery>
+    <WinnerGallery awards={awards}></WinnerGallery>
+    <GameGallery images={galleryImages}></GameGallery>
 
-    <Sponsors sponsors={mockSponsorData}></Sponsors>
+    <Sponsors sponsors={sponsors}></Sponsors>
   </div>
 </div>
 

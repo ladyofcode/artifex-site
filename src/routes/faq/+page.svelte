cddc70c292ba96b9ce8086bf972565af4f1e0235
ladyofcode <thelab@ladyofcode.com>
2024-09-10 18:12:31 +1000

    Update content and md files

---
+++ src/routes/faq/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import FAQ from '../../../content/single-pages/faq.md';
+  </script>
+
+<!-- import { metadata as rulesMetadata } from '$content/single-pages/rules.md'; -->
+
+<h1>FAQ</h1>
+
+<FAQ />
--- src/routes/game-jam/+layout.svelte
+++ src/routes/game-jam/+layout.svelte
@@ -1,8 +1,13 @@
+<script>
+    import '../../game-jam.css';
+</script>
+
 <h1>The second layout!</h1>
 
 <slot />
 
 <style>
+
     h1{
         color: black;
     }

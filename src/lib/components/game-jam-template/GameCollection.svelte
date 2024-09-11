38ae60139b4cb56301dd7191c0df7aebe21ccb5f
Yang Li <u7764210@anu.edu.au>
2024-09-11 20:31:33 +1000

    Add GameCollection component

---
+++ src/lib/components/game-jam-template/GameCollection.svelte
@@ -0,0 +1,20 @@
+<script>
+	import GameCard from "./GameCard.svelte";
+
+  export let gameData; // : { gameName: string, images: {src: string, width: number, height: number}[], members: string[], gameUrl: string, isPlayable: bool }
+</script>
+
+<div class="wrapper">
+  {#each gameData as game (game.gameName)}
+    <GameCard images={game.images} members={game.members} gameName={game.gameName} gameUrl={game.gameUrl} isPlayable={game.isPlayable}></GameCard>
+  {/each}
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    max-width: var(--gallery-width);
+  }
+</style>
--- src/lib/components/game-jam-template/PastJamTemplate.svelte
+++ src/lib/components/game-jam-template/PastJamTemplate.svelte
@@ -1,10 +1,11 @@
 <script>
-	import GameGallery from '$lib/components/game-jam-template/GameGallery.svelte';
+	import PhotoGallery from '$lib/components/game-jam-template/PhotoGallery.svelte';
 	import WinnerGallery from '$lib/components/game-jam-template/WinnerGallery.svelte';
 	import Description from './Description.svelte';
 	import Event from './Event.svelte';
 	import Sponsors from '$lib/components/game-jam-template/Sponsors.svelte';
 	import GameCard from './GameCard.svelte';
+	import GameCollection from './GameCollection.svelte';
 
 	export let jamTitle; // : string
 	export let jamTheme; // : string
@@ -13,6 +14,7 @@
 	export let awards; // : {award: string, winner: string, image: {src: string, width: number, height: number}, url: string}[]
 	export let galleryImages; // : {src: string, width: number, height: number}[]
 	export let sponsors; // : { name: string, imageSrc: string, url: string }[]
+  // export let gameData;
 
 	const mockGame = {
 		images: [
@@ -23,8 +25,119 @@
 		],
     gameName: "Can't Be Late",
     members: ['Jiayi Fan', 'Xiuchen Gu', 'Yang Li', 'Patrick Haesler', 'yeyeovo'],
-    gameUrl: 'https://www.google.com',
+    gameUrl: 'https://www.google.com',    
 	};
+
+  // Real data for Game Jam 2024
+  const gameData2024 = [
+    {
+      gameName: "Once More, With Spirit",
+      images: [
+        { src: "/game-jam/images/2024/games/once_more_with_spirit_2024.png", width: 347, height: 275 }
+      ],
+      members: ["Not recorded"],
+      gameUrl: "https://gingeh.itch.io/once-more-with-spirit",
+      isPlayable: true,
+    },
+    {
+      gameName: "12 Hours",
+      images: [
+        { src: "/game-jam/images/2024/games/12_hours_2024.png", width: 315, height: 250 }
+      ],
+      members: ["Not recorded"],
+      gameUrl: "https://itch.io/jam/cssa-artifex-anu-game-jam-2024/rate/2876792",
+      isPlayable: false,
+    },
+    {
+      gameName: "GOU: Shadow Die Thrice",
+      images: [
+        { src: "/game-jam/images/2024/games/gou_shadow_die_thrice_2024.png", width: 347, height: 332 }
+      ],
+      members: ["Xing Chen", "Hongjun Xu", "Xuancong Pang", "Tianci Li", "Shengjie Liu"],
+      gameUrl: "https://cxsgdsg.itch.io/debug-dragon",
+      isPlayable: true,
+    },
+    {
+      gameName: "Can't Be Late",
+      images: [
+        {src:'/game-jam/images/2024/games/cant_be_late_2024.png', width: 315, height: 250},
+        {src:'/game-jam/images/2024/games/cant_be_late_2024_1.png', width: 794, height: 466},
+        {src:'/game-jam/images/2024/games/cant_be_late_2024_2.png', width: 794, height: 441},
+        {src:'/game-jam/images/2024/games/cant_be_late_2024_3.png', width: 794, height: 436},
+      ],
+      members: ['Jiayi Fan', 'Xiuchen Gu', 'Yang Li', 'Patrick Haesler', 'yeyeovo'],
+      gameUrl: 'https://hitomi-kiri.itch.io/cant-be-late',
+      isPlayable: true,
+    },
+    {
+      gameName: "Caffeine Crash",
+      images: [
+        {src:'/game-jam/images/2024/games/caffeine_crash_2024.png', width: 315, height: 250},
+      ],
+      members: ["Long Feng", "Sachi Ashok", "Jay Johnston"],
+      gameUrl: 'https://itch.io/jam/cssa-artifex-anu-game-jam-2024/rate/2876783',
+      isPlayable: false,
+    },
+    {
+      gameName: "Do Catch The Gem",
+      images: [
+        {src:'/game-jam/images/2024/games/do_catch_the_gem_2024.png', width: 347, height: 188},
+      ],
+      members: ["Siqi Cheng", "Jiale Zhang", "Kris", "William"],
+      gameUrl: 'https://charrrles.itch.io/do-catch-the-gem',
+      isPlayable: true,
+    },
+    {
+      gameName: "It's Castle Time",
+      images: [
+        {src:'/game-jam/images/2024/games/its_the_castle_time_2024.png', width: 347, height: 347},
+      ],
+      members: ["Nicholas Arvanitellis", "Clare Mason-Cox", "Martyn Barac", "Emily Coper-Jones"],
+      gameUrl: 'https://inkxy.itch.io/its-castle-time',
+      isPlayable: true,
+    },
+    {
+      gameName: "Symphony of Vampires",
+      images: [
+        {src:'/game-jam/images/2024/games/symphony_of_vampires_2024.png', width: 347, height: 275},
+      ],
+      members: ["Divyesh Srivastava", "Saksham Gupta", "Aditya Iyengar", "Natalie Dunn"],
+      gameUrl: 'https://dotterine.itch.io/symphonyofvampires',
+      isPlayable: true,
+    },
+    {
+      gameName: "Ghost Hunting for Dummies",
+      images: [
+        {src:'/game-jam/images/2024/games/ghost_hunting_for_dummies_2024.png', width: 347, height: 347},
+      ],
+      members: ["Joshua King", "John Atkins", "Nikhila Gurusinghe", "Sophie Hawke", "Ryan Yoon"],
+      gameUrl: 'https://rynooy.itch.io/ghost-hunting-for-dumies',
+      isPlayable: true,
+    },
+    {
+      gameName: "Feeble Hero",
+      images: [],
+      members: ["Yonah Tal-Meyer", "Arlo Taylor"],
+      gameUrl: 'https://doomguy077.itch.io/feeble-hero',
+      isPlayable: true,
+    },
+    {
+      gameName: "Temporal Tactics",
+      images: [
+        {src:'/game-jam/images/2024/games/temporal_tactics_2024.png', width: 347, height: 347},
+      ],
+      members: ["Unknown"],
+      gameUrl: 'https://trr4m.itch.io/temporal-tactics',
+      isPlayable: true,
+    },
+    {
+      gameName: "It's Rewind Time",
+      images: [],
+      members: ["Azhar Hussain", "Jace Basden", "Adrian Carpio", "William Rogers", "Aditya Grama"],
+      gameUrl: 'https://itch.io/jam/cssa-artifex-anu-game-jam-2024/rate/2876722',
+      isPlayable: false,
+    },
+  ]
 </script>
 
 <div class="template-wrapper">
@@ -40,11 +153,11 @@
 		<Event {events} isActive={false}></Event>
 
 		<WinnerGallery {awards}></WinnerGallery>
-		<GameGallery images={galleryImages}></GameGallery>
+		<PhotoGallery images={galleryImages}></PhotoGallery>
 
 		<Sponsors {sponsors}></Sponsors>
 
-		<GameCard images={mockGame.images} gameName={mockGame.gameName} members={mockGame.members} gameUrl={mockGame.gameUrl}></GameCard>
+		<GameCollection gameData={gameData2024}></GameCollection>
     <div style="margin-block: 40px;"></div>
   </div>
 </div>
